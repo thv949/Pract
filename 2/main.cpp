0024e3cd3f2cf86d47359fc2a9edf3a031f7f332
Мария <zakazovamaria2004@gmail.com>
2023-12-23 10:18:18 +0700

    add TODO

--- 2/main.cpp
+++ 2/main.cpp
@@ -7,6 +7,7 @@
 #include "Read_Output.h"
 
 int main() {
+    _CrtGetDebugFlag(_CRTDBG_MAP_ALLOC_MEM_DF | _CRTDBG_DBG);
     setlocale(LC_ALL, "Russian");
     std::ifstream in("in.txt");
     std::vector<Menu> arr = readMenuFromFile(in);
